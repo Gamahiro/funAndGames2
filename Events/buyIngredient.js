5032ba262411f251e2b3d921b74b4be70ad26a77
Johan <jpettersen50@gmail.com>
2022-12-30 23:38:21 +0100

    integrate error messagea

--- Events/buyIngredient.js
+++ Events/buyIngredient.js
@@ -1,13 +1,16 @@
 import { itemList } from "../player/itemList.js";
 import { player } from "../player/playerObject.js";
 import { updateResourceUI } from "../controller/updateResource.js";
+import { playerError } from "../Events/errorEvent.js";
 
 function buyIngredientEvent() {
 
     for (let i = 0; i < itemList.length; i++) {
         const element = itemList[i];
-        document.querySelector(`#buy${element.ingredientName.replace(/ /g, '')}Btn`).addEventListener('click', () => {
-            if(element.shopAmount < 1) return console.log(`${element.ingredientName} is out of stock!`);
+        let btn = document.querySelector(`#buy${element.ingredientName.replace(/ /g, '')}Btn`);
+        btn.addEventListener('click', () => {
+            if(element.shopAmount < 1) return playerError(btn.parentElement, `${element.ingredientName} is out of stock!`);
+            if(player.getMoney < element.price) return playerError(btn.parentElement, `You can't afford that`);
             element.setShopAmount(-1);
             element.setPlayerAmount(1);
             player.setMoney = - element.price;
--- Events/cookMeal.js
+++ Events/cookMeal.js
@@ -1,11 +1,12 @@
 import { itemList } from "../player/itemList.js";
 import { player } from "../player/playerObject.js";
+import { playerError } from "../Events/errorEvent.js";
 
 function cookMeal(meal) {
 
     for (let i = 0; i < meal.ingredients.length; i++) {
         const element = meal.ingredients[i];
-        if(element.playerAmount < 1) return console.log(`you don't have enough ${meal.ingredients[i].ingredientName}`);
+        if(element.playerAmount < 1) return playerError(document.querySelector(`#${element.ingredientName.replace(/ /g, '')}Amount`), `you don't have enough ${meal.ingredients[i].ingredientName}`);
     }
 
     for (let i = 0; i < meal.ingredients.length; i++) {
